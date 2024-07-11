fa06ef3d41c3c47d00f2fff9a13397481fe031c5
zions <tzion12345@gmail.com>
2024-07-11 18:15:32 +0300

    support multipel pins

--- toggle_pin_output.py
+++ toggle_pin_output.py
@@ -1,27 +1,30 @@
 import time
 import RPi.GPIO as GPIO
 
-PIN = 17
-FREQUENCY_HZ = .5
+PINS = [17, 27]
+FREQUENCY_HZ = .25
 WAIT_SEC = 10
 
 def main():
-    print(f"Starting step function on pin {PIN} with frequency {FREQUENCY_HZ} Hz")
+    print(f"Starting step function on pin {PINS} with frequency {FREQUENCY_HZ} Hz")
     period = 1.0 / FREQUENCY_HZ  # Calculate the period
     high_time = period / 2  # High for half the period
     low_time = period / 2  # Low for half the period
 
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(PIN, GPIO.OUT)
+    for pin in PINS:
+        GPIO.setup(pin, GPIO.OUT)
 
     start_time = time.time()
     while (time.time() - start_time) < WAIT_SEC:
-        GPIO.output(PIN, GPIO.HIGH)  # Set pin high
-        time.sleep(high_time)  # Wait for high_time
-        GPIO.output(PIN, GPIO.LOW)  # Set pin low
-        time.sleep(low_time)  # Wait for low_time
+        for pin in PINS:
+            GPIO.output(pin, GPIO.HIGH)  # Set pin high
+            time.sleep(high_time)  # Wait for high_time
+            GPIO.output(pin, GPIO.LOW)  # Set pin low
+            time.sleep(low_time)  # Wait for low_time
 
-    GPIO.cleanup()
+    for pin in PINS:
+        GPIO.cleanup(pin)
 
 
 if __name__ == '__main__':
