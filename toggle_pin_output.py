196f8738390285c7f1b5b3042f8bc16331333519
zions <tzion12345@gmail.com>
2024-07-17 13:35:49 +0300

    argument to toggling module, wip

--- toggle_pin_output.py
+++ toggle_pin_output.py
@@ -1,13 +1,24 @@
 import time
 import RPi.GPIO as GPIO
+import argparse
 
 PINS = [17, 27]
 FREQUENCY_HZ = .2  # in high frequencies add 2% to the desired freq to compensate the pi delay
 DURATION_SEC = 20
 
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--freq', type=float, help='Frequency (Hertz) of the pulse function', default=1)
+    parser.add_argument('--duration', type=float, help='Duration in seconds', default=DURATION_SEC)
+    # parser.add_argument('')
+    args = parser.parse_args()
+    return args
+
 def main():
-    print(f"Starting step function on pin {PINS} with frequency {FREQUENCY_HZ} Hz")
-    period = 1.0 / FREQUENCY_HZ  # Calculate the period
+    args = parse_args()
+    print(f"Starting step function on pin {PINS} with frequency {args.freq} Hz")
+    period = 1.0 / args.freq  # Calculate the period
     high_time = period / 2  # High for half the period
     low_time = period / 2  # Low for half the period
 
@@ -17,7 +28,7 @@ def main():
 
     start_time = time.time()
     print("Starting cycle")
-    while (time.time() - start_time) < DURATION_SEC:
+    while (time.time() - start_time) < args.duration:
         for pin in PINS:
             GPIO.output(pin, GPIO.HIGH)  # Set pin high
         time.sleep(high_time)  # Wait for high_time
