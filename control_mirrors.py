45a66b80393bd3bd5e4136b854186ef500a53c75
zions <tzion12345@gmail.com>
2024-08-11 15:27:52 +0300

    add offset (for both axes)

--- control_mirrors.py
+++ control_mirrors.py
@@ -14,11 +14,12 @@ class MR_E_2:
     
     sysclk = 18000000
     clkdiv = 16
-    def __init__(self, bus, device, freq0, amp0, freq1=None, amp1=None):
+    def __init__(self, bus, device, freq0, amp0, freq1=None, amp1=None, offset=0.0):
         self.freq0 = freq0
         self.amp0 = amp0
         self.freq1 = freq1
         self.amp1 = amp1
+        self.offset = offset
         self.sig_gen_chnl_1 = 0x60
         if (freq1 is None) != (amp1 is None):
             raise ValueError("Both freq1 and amp1 should be set or unset")
@@ -53,6 +54,10 @@ class MR_E_2:
         ans = self.spi.set_values(self.sig_gen_chnl_1, 0x04, self.sig_gen_chnl_2, 0x04, self.amp0, self.amp1, self._flt)   # Signal-Gen Amplitude
         print(ans)
         
+        # Currently offset equals for both X and Y - may need to seperate to axes
+        ans = self.spi.set_values(self.sig_gen_chnl_1, 0x05, self.sig_gen_chnl_2, 0x05, self.offset, self.offset, self._flt)  # Offset
+        print(ans)
+
         ans = self.spi.set_values(self.sig_gen_chnl_1, 0x09, self.sig_gen_chnl_2, 0x09, 2, 2, self._int)         # External trigger set to rising edge
         print(ans)
 
@@ -72,19 +77,21 @@ def convert_polar_to_cartesian(angle_deg):
     return xy_amplitude
         
 if __name__ == '__main__':
-    DEFAULT_ANGLE_DEG = -2.531
+    DEFAULT_ANGLE_DEG = -3.4
     parser = argparse.ArgumentParser()
     parser.add_argument('--start',action='store_true', help='Start the signal generator - run until stop command')
     parser.add_argument('--stop', action='store_true', help='Stop the signal generator')
-    parser.add_argument('--debug', action='store_true', help='Pause with debugger')
+    parser.add_argument('--debug', action='store_true', help='Pause (breaking point) with debugger')
     parser.add_argument('--x', type=float, help='Angle (in degrees) of axis X', default=DEFAULT_ANGLE_DEG)
     parser.add_argument('--y', type=float, help='Angle (in degrees) of axis Y', default=DEFAULT_ANGLE_DEG)
-    parser.add_argument('--freq', type=float, help='Frequency (Hertz) of the movement of the mirror', default=1)
+    parser.add_argument('--freq', type=float, help='Frequency (Hertz) of the movement of the mirror (for both axes!)', default=1)
+    parser.add_argument('--offset', type=float, help='Offset of generated signal (for both axes!)', default=0)
     args = parser.parse_args()
 
     x_amplitude = convert_polar_to_cartesian(args.x)
     y_amplitude = convert_polar_to_cartesian(args.y)
-    mre2 = MR_E_2(bus=0, device=0, freq0=args.freq, amp0=x_amplitude, freq1=args.freq, amp1=y_amplitude)
+    offset = convert_polar_to_cartesian(args.offset)
+    mre2 = MR_E_2(bus=0, device=0, freq0=args.freq, amp0=x_amplitude, freq1=args.freq, amp1=y_amplitude, offset=offset)
 
     # mre2 = MR_E_2(bus=0, device=0, freq0=1, amp0=0.390996311772799, freq1=1, amp1=0.390996311772799)
     # mre2 = MR_E_2(bus=0, device=0, freq0=.25, amp0=0.0, freq1=0.2, amp1=0.2)
